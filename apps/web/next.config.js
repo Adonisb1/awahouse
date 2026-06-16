eb8452b64f037f2a527853e4461ab483a30b91b1
Martins-O <jojololamartins686@gmail.com>
2026-06-16 17:20:51 +0100

    fix(prisma): revert custom output and standalone build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,7 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
-  output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../../'),
   transpilePackages: ['@awahouse/db', '@awahouse/types'],
   images: {
     remotePatterns: [
